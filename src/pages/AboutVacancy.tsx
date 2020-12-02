bc73b355c85e9333109b0fd4bf0e1c61b8a4f751
Alexander Glazyrin <glazyrin-ai@yandex.ru>
2020-12-02 20:43:23 +0300

    changed readme.md

--- src/pages/AboutVacancy.tsx
+++ src/pages/AboutVacancy.tsx
@@ -1,7 +1,7 @@
 import React, {useContext, useEffect} from 'react';
 import {observer} from "mobx-react-lite";
 import {JobsContext} from "../App";
-import {useParams} from 'react-router-dom'
+import {Link, useParams} from 'react-router-dom'
 import parse from "html-react-parser"
 import {Typography} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
@@ -9,7 +9,7 @@ import {getSalary} from "../components/VacancyCard";
 
 const useStyles = makeStyles({
   title: {
-    marginTop: 30,
+    marginTop: 20,
     marginBottom: 20,
   },
   text: {
@@ -24,7 +24,7 @@ const useStyles = makeStyles({
 export const AboutVacancy: React.FC = observer(() => {
   const classes = useStyles();
   const store = useContext(JobsContext)?.vacancyInfoStore;
-  const params: {id: string} = useParams();
+  const params: { id: string } = useParams();
 
   useEffect(() => {
     store?.loadInfo(params.id)
@@ -32,14 +32,18 @@ export const AboutVacancy: React.FC = observer(() => {
 
   return (
     <>
-      <Typography className={classes.title} variant="h5" >
+      <div style={{marginTop: 20}}>
+        <Link className={classes.link} to="/">Назад</Link>
+      </div>
+      <Typography className={classes.title} variant="h5">
         {store?.vacancyInfo.name}
       </Typography>
       <Typography className={classes.text} variant="h5">
         {getSalary(store ? store.vacancyInfo.salary : null)}
       </Typography>
       <Typography variant="h6">
-        <a className={classes.link} href={store?.vacancyInfo.employer.url}>{store?.vacancyInfo.employer.name.toUpperCase()}</a>
+        <a className={classes.link}
+           href={store?.vacancyInfo.employer.url}>{store?.vacancyInfo.employer.name.toUpperCase()}</a>
       </Typography>
       <Typography className={classes.text}>
         {store?.vacancyInfo.area}
